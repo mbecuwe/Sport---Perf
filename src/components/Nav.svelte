e5e7e369f0da96a9edcdf9acf6276fa0199572de
mbecuwe <31662467+mbecuwe@users.noreply.github.com>
2020-05-26 13:53:56 +0200

    commit

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -58,6 +58,7 @@
 		<li><a aria-current='{segment === "performance" ? "page" : undefined}' href='performance'>performance</a></li>
 
 		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+		<li><a aria-current='{segment === "tutorial" ? "page" : undefined}' href='tutorial'>tutorial</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
