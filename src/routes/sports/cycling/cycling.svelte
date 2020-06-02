df42a5b53948ccef123666747f8102fd6de36839
muhuuh <64065365+muhuuh@users.noreply.github.com>
2020-06-02 22:42:40 +0200

    layout cycling commit_4

--- src/routes/sports/cycling/cycling.svelte
+++ src/routes/sports/cycling/cycling.svelte
@@ -125,13 +125,13 @@ const nb_displayed = 3
 
 </style>
 <div class="mb-20">
-    <div class="mb-6 shadow">
+    <div class="mb-6 shadow ">
         <img src="bycicle2.jpg" alt="cycling" class="object-cover h-48 w-full shadow rounded-md">
     </div>
 
 
     <div class="flex justify-center mb-16 bg-blue-700 border-2 border-gray-300 shadow-lg rounded-t-md">
-        <div class="text-center p-2 text-3xl text-blue-700 font-semibold bg-blue-100 px-8 tracking-widest">
+        <div class="font-sans Arial text-center p-2 text-3xl text-blue-700 font-semibold bg-blue-100 px-8 tracking-widest">
             Cycling Performance
         </div>
     </div>
