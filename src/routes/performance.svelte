60dcded3019a16d878e23f910265e6ece58ac876
muhuuh <64065365+muhuuh@users.noreply.github.com>
2020-06-03 16:34:56 +0200

    layout performance commit_3

--- src/routes/performance.svelte
+++ src/routes/performance.svelte
@@ -10,36 +10,36 @@
 
 	<div class="md:flex justify-center mb-8">
 		<div class="text-center mx-4 md:w-1/2">
-			<p class='font-semibold  text-xl  italic text-blue-700 pb-6'>Cycling</p>
+			<p class='font-semibold tracking-wider text-xl text-blue-700 pb-6'>Cycling</p>
 			<div class="hover:shadow-2xl hover:border-blue-800">
 				<a href='sports/cycling/cycling'>
-				<img src="bycicle21.jpg" alt="cycling">
+				<img src="bycicle21.jpg" alt="cycling"  class="rounded">
 				</a>
 			</div>
 		</div>
 		<div class=" text-center mx-4  md:w-1/2 ">
-			<p class='font-semibold  text-xl  italic text-blue-700 pb-6'>Running</p>
+			<p class='font-semibold tracking-wider text-xl text-blue-700 pb-6'>Running</p>
 			<div class="hover:shadow-2xl hover:border-blue-800">
 				<a href='sports/running/running'>
-				<img src="running1.jpg" alt="running">
+				<img src="running1.jpg" alt="running"  class="rounded">
 				</a>
 			</div> 
 		</div>
 	</div>
 	<div class="md:flex justify-center">
 		<div class=" text-center mx-4 md:w-1/2">
-			<p class='font-semibold  text-xl  italic text-blue-700 pb-6'>Work-out</p>
+			<p class='font-semibold tracking-wider text-xl text-blue-700 pb-6'>Work-out</p>
 			<div class="hover:shadow-2xl hover:border-blue-800">
 				<a href='sports/workout/workout'>
-				<img src="swimming3.jpg" alt="workout">
+				<img src="workout3.jpg" alt="workout" class="rounded">
 				</a>
 			</div>
 		</div>
 		<div class=" text-center mx-4  md:w-1/2 ">
-			<p class='font-semibold  text-xl  italic text-blue-700 pb-6'>Swimming</p>
+			<p class='font-semibold tracking-wider text-xl text-blue-700 pb-6'>Swimming</p>
 			<div class="hover:shadow-2xl hover:border-blue-800">
 				<a href='sports/swimming/swimming'>
-				<img src="swimming2.jpg" alt="swimming">
+				<img src="swimming2.jpg" alt="swimming" class="rounded">
 				</a>
 			</div>
 		</div>
