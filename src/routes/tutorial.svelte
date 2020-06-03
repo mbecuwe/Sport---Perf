a7a39889dc60be68296f2814b42a1d740d0ee5bc
muhuuh <64065365+muhuuh@users.noreply.github.com>
2020-06-03 10:06:05 +0200

    layout tutorial commit_1

--- src/routes/tutorial.svelte
+++ src/routes/tutorial.svelte
@@ -1,24 +1,23 @@
 
 
-<body class="bg-blue-100 w-screen h-screen m-8">
-        <div class="m-8">
-            <div class="flex justify-center">
-                <h1 class="font-bold text-4xl text-blue-800">Tutorial</h1>
-            </div>
-            <div class=" mt-12 items-center justify-center"> 
-                <div class="container mx-auto py-16"> 
-                    <p class="flex justify-center mb-6 text-gray-500 text-xl">Where it all starts</p>
-                    <div class="flex justify-center pb-6 mx-auto"> 
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/6uG9lsx-EMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls autoplay></iframe>   
-                    </div>
-                </div>
-                <div class="container mx-auto py-16"> 
-                    <p class="flex justify-center mb-6 text-gray-500 text-xl">How to use the application</p>
-                    <div class="flex justify-center pb-6 mx-auto"> 
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/6uG9lsx-EMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls autoplay></iframe>   
-                    </div>
-                </div>
+
+<div class="m-8">
+    <div class="flex justify-center">
+        <h1 class="text-4xl text-blue-700 font-semibold tracking-widest">Tutorial</h1>
+    </div>
+    <div class=" mt-12 xl:flex justify-around"> 
+        <div class=" m-16"> 
+            <p class="flex justify-center mb-6 font-semibold mb-6 text-xl text-blue-800">Where it all starts</p>
+            <div class="flex justify-center pb-6 "> 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/6uG9lsx-EMo?autoplay=1&loop=1&autopause=1" frameborder="0" allow="fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls autoplay></iframe>   
             </div>
         </div>
+        <div class="m-16"> 
+            <p class="flex justify-center mb-6 font-semibold mb-6 text-xl text-blue-800">How to use the application</p>
+            <div class="flex justify-center pb-6 "> 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/6uG9lsx-EMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen controls autoplay></iframe>   
+            </div>
+        </div>
+    </div>
+</div>
 
-    </body>
