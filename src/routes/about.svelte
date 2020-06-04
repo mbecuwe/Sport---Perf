c26afc1cd4ee4363606b29a334512545d0c54dfe
muhuuh <64065365+muhuuh@users.noreply.github.com>
2020-06-04 13:17:36 +0200

    Layout contact commit3

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,19 +5,19 @@
 </style>
 
 <div class="mb-6 shadow ">
-    <img src="work_hader.jpg" alt="work harder sign" class="antialiased bg-gray-600 object-cover h-48 w-full shadow rounded-md">
+    <img src="work_hader.jpg" alt="work harder sign" class="antialiased bg-gray-600 object-cover h-48 w-full shadow rounded-md opacity-75">
 </div>
-
+<!--inspired from https://tailwindcomponents.com/component/profile-card-with-image-background-->
     <div class="flex justify-center mb-8">
         <h1 class="text-4xl text-blue-700 font-semibold tracking-widest">About Us</h1>
     </div>
 
     <div class="md:flex justify-center">
         <div class="m-2 rounded-lg shadow-lg bg-gray-600 w-full flex flex-row flex-wrap p-3 antialiased" style="
-        background-image: url('https://images.unsplash.com/photo-1578836537282-3171d77f8632?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+        background-image: url('https://images.unsplash.com/photo-1500856056008-859079534e9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80');
         background-repeat: no-repat;
         background-size: cover;
-        background-blend-mode: multiply ;
+        background-blend-mode: ;
         ">
             <div class="w-1/3">
                 <img class="rounded-lg shadow-lg antialiased" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">  
@@ -26,7 +26,7 @@
                 <div class="w-full text-right text-gray-700 font-semibold relative pt-3 md:pt-0">
                 <div class="text-2xl text-white leading-tight">Martin</div>
                 <div class="text-normal text-gray-300 hover:text-gray-400 cursor-pointer"><span class="border-b border-dashed border-gray-500 pb-1">Death Star Supervisor</span></div>
-                <div class="text-sm text-gray-300 hover:text-gray-400 cursor-pointer md:absolute pt-3 md:pt-0 bottom-0 right-0">Last Seen: <b>2 days ago</b></div>
+                <div class="text-sm text-gray-300 hover:text-gray-400 cursor-pointer md:absolute pt-3 md:pt-0 bottom-0 right-0">Last Seen: <b>3 days ago</b></div>
                 </div>
             </div>
         </div>  
@@ -50,7 +50,7 @@
     </div>
 
     <div class="mt-16 mb-8 text-center text-2xl text-blue-800"> Do you have questions?</div>
-
+    <!-- contact form from https://tailwindcomponents.com/component/contact-form-->
     <form id="contact-me" class="w-full mx-auto max-w-3xl bg-white shadow p-8 text-gray-700 ">
 
 
